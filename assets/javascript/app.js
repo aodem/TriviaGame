dc192007bdba6bd38f75b86ca2382c21ff2f262b
debarge <43295961+aodem@users.noreply.github.com>
2018-12-04 07:12:11 -0700

    push 1.3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,50 @@
 //The JS for the Denzel Washington trivia game.
 
+var intervalId;
+
 var trivia = {
-    qOne: {
+    objarray: [{
         question: "'Creasy bear' was the name of a young girls teddy bear in what Denzel movie? ",
         possibleAnswers: ["The Equalizer", "Training Day", "Man On Fire", "John Q"],
-        correctAnswer: () => { return trivia.qOne.possibleAnswers[2] },
-    },
-    qTwo: {
+        correctAnswer: () => { return trivia.qOne.possibleAnswers[2] }
+        },
+        {
         question: "Denzel played a rogue intelligence agent in this movie that ended in him helping the agent tasked to bring him in.",
         possibleAnswers: ["Safe House", "The Taking of Pelam 123", "Deja Vu", "The Book of Eli", "Inside Man"],
-        correctAnswer: () => { return trivia.qTwo.possibleAnswers[0] },
-    },
-    qThree: {
-        question: "'King King ain't got nothing on me' is a famous line from this movie.",
-        possibleAnswers: ["The Equalizer 2", "Fences", "Flight", "Training Day"],
-        correctAnswer: () => { return trivia.qThree.possibleAnswers[3] },
-    },
-    qFour: {
+        correctAnswer: () => { return trivia.qTwo.possibleAnswers[0] }
+        },
+        {
         question: "Denzel played a character that held a hospital hostage in which movie?",
         possibleAnswers: ["The Mancharian Candidate", "John Q", "2 Guns", "American Gangster"],
-        correctAnswer: () => { return trivia.qFour.possibleAnswers[1] },
-    },
-    qFive: {
+        correctAnswer: () => { return trivia.qFour.possibleAnswers[1] }
+        },
+        {
+        question: "'King King ain't got nothing on me' is a famous line from this movie.",
+        possibleAnswers: ["The Equalizer 2", "Fences", "Flight", "Training Day"],
+        correctAnswer: () => { return trivia.qThree.possibleAnswers[3] }
+        },
+        {
         question: "In this movie, Denzels character coached a football team that was lead by a Quarterback that went by the nickname 'Sunshine'.",
         possibleAnswers: ["Rember the Titans", "The Great Debaters", "unstoppable", "Inside Man"],
-        correctAnswer: () => { return trivia.qFive.possibleAnswers[0] },
-    },
-    qSix: {
+        correctAnswer: () => { return trivia.qFive.possibleAnswers[0] }
+        },
+        {
         question: "NBA all star Ray Allen played the son of Jake Shuttlesworth played by Denzel Washington",
         possibleAnswers: ["Antwon Fisher", "He Got Game", "John Q"],
-        correctAnswer: () => { return trivia.qSix.possibleAnswers[1] },
-    },
-    qSeven: {
+        correctAnswer: () => { return trivia.qSix.possibleAnswers[1] }
+        },
+        {
         question: "Mark Wahleberg co-starred in this movie",
         possibleAnswers: ["2 Guns", "The Equalizer", "Fences", "The Equalizer 2"],
-        correctAnswer: () => { return trivia.qSeven.possibleAnswers[0] },
-    },
-    qEight: {
+        correctAnswer: () => { return trivia.qSeven.possibleAnswers[0] }
+        },
+        {
         question: "Did he really land a plane after turning it upside down and flying intoxicated?",
         possibleAnswers: ["Flight", "Unstoppable", "Inside Man", "Fences"],
-        correctAnswer: () => { return trivia.qEight.possibleAnswers[0] },
-    },
+        correctAnswer: () => { return trivia.qEight.possibleAnswers[0] }
+        },
+    ],
+    indexslide: 0,   
     questionSlide: function (value1, value2) {
         function myQDiv (innerContent) {
             a = $("<div>").appendTo("#slide_entry");
@@ -68,12 +72,22 @@ var trivia = {
             console.log(1 < 0);
         }
     },
+    slideChange: function(){
+        var slideObj = trivia.objarray[trivia.indexslide];
+        console.log(slideObj);
+        trivia.questionSlide(slideObj.question, slideObj.possibleAnswers);
+        trivia.indexslide++
+        console.log(trivia.indexslide);
+    },
+    interval: function() {
+        trivia.slideChange();
+        intervalId = setInterval(trivia.slideChange, 5000);
+    }
 };
+
 
 
 $(document).on("click", "button", function () {
     trivia.answerCheck($(this), trivia.qEight);
 })
-
-
 
